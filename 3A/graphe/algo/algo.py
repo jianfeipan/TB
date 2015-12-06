1ce51fbac79b684463d467a56440c18d55934d85
jianfeipan <jianfei.pan.tb@gmail.com>
2015-12-06 14:37:15 +0100

    floyd graphe

--- 3A/graphe/algo/algo.py
+++ 3A/graphe/algo/algo.py
@@ -14,11 +14,11 @@ from priodict import priorityDictionary
 
 class Algo:
 	@staticmethod
-	def Dijkstra(graphe, sourceNode):
+	def __DijkstraFromSource(graph, sourceNode):
 		utils.Utils.isStr(sourceNode)
-		utils.Utils.isDict(graphe)
-		if sourceNode not in graphe:
-			raise ValueError('the source node is not in the graphe')
+		utils.Utils.isDict(graph)
+		if sourceNode not in graph:
+			raise ValueError('the source node is not in the graph')
 
 		D = {}	# dictionary of final distances
 		P = {}	# dictionary of predecessors
@@ -27,22 +27,61 @@ class Algo:
 	
 		for anode in Q:
 			D[anode] = Q[anode]
-			graphe[sourceNode].routingPath[anode]=[]
+			graph[sourceNode].routingPath[anode]=[]
 		
-			for neighbor in graphe[anode].connections:
-				sourceToNeighborLenght = D[anode] + graphe[anode].connections[neighbor]
+			for neighbor in graph[anode].connections:
+				sourceToNeighborLenght = D[anode] + graph[anode].connections[neighbor]
 				if neighbor in D:
 					if sourceToNeighborLenght < D[neighbor]:
 						raise ValueError, "Dijkstra: found better path to already-final vertex"
 				elif neighbor not in Q or sourceToNeighborLenght < Q[neighbor]:
 					Q[neighbor] = sourceToNeighborLenght
 					P[neighbor] = anode
-			graphe[sourceNode].routingPath[anode].append(Q[anode])	
+			graph[sourceNode].routingPath[anode].append(Q[anode])	
 			end = anode;
 			while 1:
-				graphe[sourceNode].routingPath[anode].append(end)
+				graph[sourceNode].routingPath[anode].append(end)
 				if end == sourceNode: break
 				end = P[end]				
-			graphe[sourceNode].routingPath[anode].reverse()
-			print graphe[sourceNode].routingPath[anode]
+			graph[sourceNode].routingPath[anode].reverse()
 
+	@staticmethod	
+	def Dijkstra(graph):	
+		for key in graph:
+			Algo.__DijkstraFromSource(graph,key)
+
+	@staticmethod
+	def Floyd_Warshall(graph):
+		utils.Utils.isDict(graph)
+
+		dist = {}
+		pred = {}
+		for u in graph:
+		    dist[u] = {}
+		    pred[u] = {}
+		    for v in graph:
+		        dist[u][v] = 1000
+		        pred[u][v] = -1
+		    dist[u][u] = 0
+		    for neighbor in graph[u].connections:
+		        dist[u][neighbor] = graph[u].connections[neighbor]
+		        pred[u][neighbor] = u
+	 
+		for t in graph:
+		    for u in graph:
+		        for v in graph:
+		            newdist = dist[u][t] + dist[t][v]
+		            if newdist < dist[u][v]:
+		                dist[u][v] = newdist
+		                pred[u][v] = pred[t][v]
+		for s in graph:			
+			for d in graph:
+				graph[s].routingPath[d]=[]
+				end = d;
+				graph[s].routingPath[d].append(dist[s][d])
+				while 1:
+					graph[s].routingPath[d].append(end)
+					if end == s: break
+					end = pred[s][end]				
+				graph[s].routingPath[d].reverse()
+				
--- 3A/graphe/algo/test_algo.py
+++ 3A/graphe/algo/test_algo.py
@@ -16,8 +16,8 @@ class TestAlgo(unittest.TestCase):
 		with self.assertRaises(TypeError):
 			algo.Algo.Dijkstra([],'192.168.0.1')
 
-	#oriented graphe
-	def test_algo_complets_the_routingPath_of_the_sourceNode(self):
+	# Dijkstra : oriented graphe
+	def test_algo_Dijkstra_complets_the_routingPath_of_the_sourceNode(self):
 		self.graphe= {}
 
 		self.nodeS = node.Node();
@@ -40,11 +40,14 @@ class TestAlgo(unittest.TestCase):
 						'v': self.nodeV,
 						'x':self.nodeX,
 						'y':self.nodeY}
-		for key in self.graphe:
-			algo.Algo.Dijkstra(self.graphe, key)	
 
-	#non oriented graphe
-	def test_algo_complets_the_routingPath_of_the_sourceNode(self):
+		algo.Algo.Dijkstra(self.graphe)
+		for key in self.graphe:			
+			print key + ":"
+			print self.graphe[key].routingPath	
+
+	# Dijkstra : non oriented graphe
+	def test_algo_dijkstra_complets_the_routingPath_of_the_sourceNode2(self):
 		self.graphe= {}
 
 		self.nodeA = node.Node();
@@ -63,8 +66,64 @@ class TestAlgo(unittest.TestCase):
 						'b': self.nodeB,
 						'c': self.nodeC,
 						'd':self.nodeD}
+		algo.Algo.Dijkstra(self.graphe)
 		for key in self.graphe:
-			algo.Algo.Dijkstra(self.graphe, key)	
+			print key + ":"
+			print self.graphe[key].routingPath	
+	# Floyd Warshall : oriented graphe
+	def test_algo_Floyd_Warshall_complets_the_routingPath_of_the_sourceNode(self):
+		self.graphe= {}
+
+		self.nodeS = node.Node();
+		self.nodeS.connections = {'u':10.0, 'x':5.0};			
+
+		self.nodeU = node.Node();
+		self.nodeU.connections= {'v':1.0, 'x':2.0};
+
+		self.nodeV = node.Node();
+		self.nodeV.connections= {'y':4.0};
+
+		self.nodeX = node.Node();
+		self.nodeX.connections= {'u':3.0,'v':9.0,'y':2.0};
+
+		self.nodeY = node.Node();
+		self.nodeY.connections = {'s':7.0,'v':6.0};
+		
+		self.graphe= {'s': self.nodeS,
+						'u': self.nodeU,
+						'v': self.nodeV,
+						'x':self.nodeX,
+						'y':self.nodeY}
+
+		algo.Algo.Floyd_Warshall(self.graphe)
+		for key in self.graphe:			
+			print key + ":"
+			print self.graphe[key].routingPath	
+
+	# Floyd Warshall  : non oriented graphe
+	def test_algo_Floyd_Warshall_complets_the_routingPath_of_the_sourceNode2(self):
+		self.graphe= {}
+
+		self.nodeA = node.Node();
+		self.nodeA.connections = { 'b':1.0,'c':5.0, 'd':2.0};			
+
+		self.nodeB = node.Node();
+		self.nodeB.connections= {'a':1.0,'c':4.0, 'd':6.0};
+
+		self.nodeC = node.Node();
+		self.nodeC.connections= {'a':5.0, 'b':4.0, 'd':2.5};
+
+		self.nodeD = node.Node();
+		self.nodeD.connections= {'a':2.0, 'b':6.0,'c':2.5};
+		
+		self.graphe= {'a': self.nodeA,
+						'b': self.nodeB,
+						'c': self.nodeC,
+						'd':self.nodeD}
+		algo.Algo.Floyd_Warshall(self.graphe)
+		for key in self.graphe:
+			print key + ":"
+			print self.graphe[key].routingPath			
 
 if __name__ == '__main__':
 	unittest.main()
