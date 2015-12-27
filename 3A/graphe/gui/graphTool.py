3147c2e85bbbf257780e93e4bb3695aeee87e757
jianfeipan <jianfei.pan.tb@gmail.com>
2015-12-28 00:59:07 +0100

    added Tkinter gui and graphtool demos

--- 3A/graphe/gui/graphTool.py
+++ 3A/graphe/gui/graphTool.py
@@ -5,7 +5,7 @@ def main():
     v1 = g.add_vertex()
     v2 = g.add_vertex()
     e = g.add_edge(v1, v2)
-    graph_draw(g, vertex_text=g.vertex_index, vertex_font_size=18)
+    graph_draw(g, vertex_text=g.vertex_index, vertex_font_size=10)
 
 if __name__ == "__main__":
     main()
