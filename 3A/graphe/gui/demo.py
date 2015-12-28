a70d91aa1528172052602c9946192064bea21454
jianfeipan <jianfei.pan.tb@gmail.com>
2015-12-28 12:31:30 +0100

    find gui

--- 3A/graphe/gui/demo.py
+++ 3A/graphe/gui/demo.py
@@ -1,11 +1,29 @@
-from graph_tool.all import *
-
+from Tkinter import *        
+class Nodeslist(Listbox):
+	def __init__(self, parent):
+		Listbox.__init__(self,parent,height=100)
+		self.initUI()
+	def initUI(self):
+		li = 'Carl Patrick Lindsay Helmut Chris Gwen'.split()
+		for item in li:
+			self.insert(0,item)
+		self.pack(side=LEFT)
+    	
+class NodeslistFrame(Frame):
+	def __init__(self, parent):
+		Frame.__init__(self, parent,padx=5,pady=5)
+		self.initUI()
+		
+	def initUI(self):
+		Label(self, text="Nodes:",anchor = NW).pack(fill=X)
+		self.pack(side=LEFT)
+    	
+        
 def main():
-    g = Graph()
-    v1 = g.add_vertex()
-    v2 = g.add_vertex()
-    e = g.add_edge(v1, v2)
-    graph_draw(g, vertex_text=g.vertex_index, vertex_font_size=18)
+    root = Tk()
+    root.geometry("1000x600+30+30")
+    nodesList = Nodeslist(NodeslistFrame(root))
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
